14961ef10f248d02c0ec76121547e926871d26b5
DaveBruwer <davebruwer@gmail.com>
2021-01-17 13:56:56 +1300

    updated xor_gate to be more realistic

--- main.py
+++ main.py
@@ -55,24 +55,18 @@ class or_gate:
 class xor_gate:
 
     def __init__(self, _signal1=False, _signal2=False):
-        self.t1 = transistor(_signal1)
-        self.t2 = transistor(_signal2)
-        if self.t1.get_state() and self.t2.get_state():
-            self.state = False
-        elif self.t1.get_state() or self.t2.get_state():
-            self.state = True
-        else:
-            self.state = False
+        self.a1 = and_gate(_signal1, _signal2)
+        self.o1 = or_gate(_signal1, _signal2)
+        self.n1 = not_gate(self.a1.get_state())
+        self.a2 = and_gate(self.n1.get_state(), self.o1.get_state())
+        self.state = self.a2.get_state()
 
     def send_signal(self, _signal1, _signal2):
-        self.t1.send_signal(_signal1)
-        self.t2.send_signal(_signal2)
-        if self.t1.get_state() and self.t2.get_state():
-            self.state = False
-        elif self.t1.get_state() or self.t2.get_state():
-            self.state = True
-        else:
-            self.state = False
+        self.a1.send_signal(_signal1, _signal2)
+        self.o1.send_signal(_signal1, _signal2)
+        self.n1.send_signal(self.a1.get_state())
+        self.a2.send_signal(self.n1.get_state(), self.o1.get_state())
+        self.state = self.a2.get_state()
 
     def get_state(self):
         return self.state
